b52744256b2bd3b93a9bcb12c2b24fdbc159d2a3
Sasiraj-G <sasirajg2005@gmail.com>
2024-06-27 14:56:00 +0530

    modify

--- reverseNumber.kt
+++ reverseNumber.kt
@@ -1,3 +1,19 @@
+
+fun reverseNumbers(num : Int):Int{
+    var temp : Int? : null
+    var sum : Int? : null
+    while (num != 0){
+        temp=num % 10
+        sum=(sum*10)+temp
+        num/=10
+    }
+    return sum
+
+
+}
+
+
 fun main(){
-    println("hello")
+   var res=reverseNumbers(12345)
+    println(res)
 }
